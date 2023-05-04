9b01b4f465bf984db5fa7be34cc421c20ad1ec67
cassianogatto <cassianogatto@gmail.com>
2023-05-04 18:36:04 -0300

    intro comunidades rmd

--- Dabukuri_espacial/app.R
+++ Dabukuri_espacial/app.R
@@ -1,7 +1,8 @@
-
+# setwd("C:/Users/Cliente/Documents/Cassiano/Shiny/Memorial_markdown-shiny/Memorial_descritivo/")
 
 library(shiny)
 library(bslib)
+library(dplyr)
 
 # https://appsilon.com/r-shiny-bslib/
 custom_theme <- bs_theme(
@@ -12,35 +13,43 @@ custom_theme <- bs_theme(
 
 ui <- navbarPage(
     
-    theme = bs_theme(version = 5, bootswatch = "lumen"),#custom_theme,
+    # theme = bs_theme(version = 5, bootswatch = "lumen"),#custom_theme,
     
     title = "DABUKURI - Direito ao Território", collapsible = TRUE, 
     
-    tabPanel("Comunidade",
+    tabPanel("Comunidade", icon = tags$img( src = "DABUKURI.jpg" ),
              
-             fluidPage(
-                 theme = bs_theme(version = 5, bootswatch = "lumen"),
-                 fluidRow(
-                     
-                    column(4,
-
-                        fileInput( inputId = "filetab",  label = "Comunidade (tabela '.csv') ", accept = c(".csv"), multiple=TRUE),
-                        
-                        selectInput(inputId = "memorial_template", label = "Arquivo markdown template", 
-                                       choices = dir()[dir() %>% grep(pattern = ".Rmd")],
-                                       selected = "template_memorial_4_SHINY.Rmd"),
-                            
-                        selectInput(inputId = "topografico_template", label = "Topográfico template", 
-                                       choices = dir()[dir() %>% grep(pattern = ".Rmd")],
-                                       selected = "topografico_template.Rmd"),
-                    ),
+        fluidPage(
            
-                    column(8,
-                           
-                        tags$h3("Tabela Geral"),  tableOutput("tab"),
-                    ),
-                 )
-             ),
+            theme = bs_theme(version = 5, bootswatch = "lumen"),
+             
+            fluidRow(
+                 
+                column(4,
+                       
+                    radioButtons('comunidade', 'Escolha a comunidade', c('Kokama', 'Ipixuna'), inline = TRUE),
+
+                    fileInput( inputId = "filetab",  label = "Comunidade (tabela '.csv') ", accept = c(".csv"), multiple=TRUE),
+                    
+                    selectInput(inputId = "memorial_template", label = "Arquivo markdown template", 
+                                   choices = dir()[dir() %>% grep(pattern = ".Rmd")],
+                                   selected = "template_memorial_4_SHINY.Rmd"),
+                        
+                    selectInput(inputId = "topografico_template", label = "Topográfico template", 
+                                   choices = dir()[dir() %>% grep(pattern = ".Rmd")],
+                                   selected = "topografico_template.Rmd"),
+                ),
+       
+                column(8,
+                       
+                    uiOutput("comunidade_intro"),
+                    
+                    tags$img( src = "www/ipixuna_satelite.png" ),
+                       
+                    tags$h3("Tabela Geral"),  tableOutput("tab"),
+                ),
+             )
+         ),
              
 
     ),
@@ -53,20 +62,28 @@ ui <- navbarPage(
    ),
     navbarMenu("Documentos",
              
-          tabPanel("Memorial", 
-                      
-                      tags$h3("o arquivo gerado está em:"),
+          tabPanel("Memorial",
+                    
+                    tags$h3("o arquivo gerado está em:"),
                         
-                      uiOutput("markdown_memorial"),
+                    uiOutput("markdown_memorial"),
+
+                    tags$h4("preview:"),
+                    
+                    #includeHTML("C:/Users/Cliente/Documents/Cassiano/Shiny/Memorial_markdown-shiny/Memorial_descritivo/Dabukuri_espacial/outputs/memorial_casa_55.html"),
                       
-                      htmlOutput("preview")
+                    htmlOutput("preview_memorial")
           ),
             
           tabPanel("Topográfico",
                       
-                      tags$h3("o arquivo gerado está em:"),
+                    tags$h3("o arquivo gerado está em:"),
                       
-                      uiOutput("markdown_topografico"),
+                    uiOutput("markdown_topografico"),
+                    
+                    tags$h4("preview:"),
+                   
+                    htmlOutput("preview_topografico")
           ),
   )
 )
@@ -97,8 +114,10 @@ server <- function(input, output, session) {
   
   output$tab <- renderTable({  tab_react() %>% select(1:5) })
   
-  output$V_tab <- renderTable(  tab_react() %>% select(1:12) %>% slice( which(tab_react()$id == input$lista_de_id) )  )
+  output$V_tab <- renderTable(  tab_react() %>% select(id, nome, cpf, rua, casa, contains("dist_"), escala, observacoes, area, perim) %>% slice( which(tab_react()$id == input$lista_de_id) )  )
   
+  output$comunidade_intro <- renderUI({ includeMarkdown( switch(input$comunidade, Kokama = "www/kokama_intro.Rmd", Ipixuna = "www/ipixuna_intro.Rmd") ) })
+            
   # SALVE MEMORIAL
   
   output$markdown_memorial <- renderUI( {
@@ -119,11 +138,64 @@ server <- function(input, output, session) {
                       params = list(tab = tab_react(), casa = input$lista_de_id, V = V_react() )  )
   })
   
-  # output$preview <- renderUI({   function(){  return( includeHTML( paste0( "memorial_casa_", input$casa,".html") ) )  } })
+  # works only with the character for the whole file path... trying to make the paste0 works... it never ends...
+  output$preview_memorial <- renderUI({  includeHTML( 
+      
+      #paste0(getwd(),"/outputs/memorial_casa_", input$lista_de_id) 
+      
+      "C:/Users/Cliente/Documents/Cassiano/Shiny/Memorial_markdown-shiny/Memorial_descritivo/Dabukuri_espacial/outputs/memorial_casa_56.html"
+      
+      ) })   # "C:/Users/Cliente/Documents/Cassiano/Shiny/Memorial_markdown-shiny/Memorial_descritivo/Dabukuri_espacial/outputs/memorial_casa_55.html" )
+
+  
+  output$preview_topografico <- renderUI({  includeHTML( paste0(getwd(),"/outputs/topografico_casa_", input$lista_de_id) ) })
+  
 }
 
 
-shinyApp(ui = ui, server = server, options = list(width = 100))
+# runApp('Dabukuri_espacial')
 
-# Run the application with themer 
+shinyApp( ui = ui, server = server, options = list(width = 100) )
+
+# Run the application with themer
 # run_with_themer( shinyApp( ui = ui, server = server, options = list(width = 100) ) )
+
+
+
+
+
+# load figures in Mdown through R
+
+# ```{r, out.width=='350px', fig.align='right'}
+# 
+# figure_path <- paste0('figures/casa__', V$id, '.png')  ## CHECK FIGURES' ID !!
+# 
+# knitr::include_graphics(figure_path)
+# 
+# ```
+
+# Download
+# output$downloadReport <- downloadHandler(
+#     filename = function() {
+#         paste('my-report', sep = '.', switch(
+#             input$format, PDF = 'pdf', HTML = 'html', Word = 'docx'
+#         ))
+#     },
+#     
+#     content = function(file) {
+#         src <- normalizePath('report.Rmd')
+#         
+#         # temporarily switch to the temp dir, in case you do not have write
+#         # permission to the current working directory
+#         owd <- setwd(tempdir())
+#         on.exit(setwd(owd))
+#         file.copy(src, 'report.Rmd', overwrite = TRUE)
+#         
+#         library(rmarkdown)
+#         out <- render('report.Rmd', switch(
+#             input$format,
+#             PDF = pdf_document(), HTML = html_document(), Word = word_document()
+#         ))
+#         file.rename(out, file)
+#     }
+# )
