860068ec96dcf9f42ffe848936d5c0269e8533d0
Cassiano <cassianogatto@gmail.com>
2023-05-22 22:49:32 -0400

    espero que não haja conflitos - 4 dias atrás

--- Dabukuri_espacial/app.R
+++ Dabukuri_espacial/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(bslib)
 library(tidyverse)
+library(DT)
 # source("html_select_module_small.R")
 
 # https://appsilon.com/r-shiny-bslib/
@@ -89,7 +90,7 @@ ui <-
                     
                     tags$h5("verifique os dados e escolha uma casa (id)"),
                     
-                    tableOutput("tab"), 
+                    DTOutput("tab"), 
                 )
         ),
         
@@ -248,7 +249,7 @@ server <- function(input, output, session) {
             
             validate(need(ext == "csv", "Please upload a csv file"))
             
-            tab <- read.csv(file$datapath, header = TRUE) %>% as_tibble()
+            tab <- read.csv(file$datapath, header = TRUE, check.names = FALSE) %>% as_tibble()
             
         } else {
             
@@ -267,7 +268,7 @@ server <- function(input, output, session) {
         V <- tab_react() %>% slice(row_slice) 
     }  )
     
-    output$tab <- renderTable( {  tab_react() # %>%  select(id, nome, cpf, rua, casa, area, perim, dist_frente, dist_lateral, observacoes, obs_frente, obs_lat_dir, obs_lat_esq, obs_fundos, X1 = dms_x_M01, Y1 = dms_y_M01, X2 = dms_x_M02, Y2 = dms_y_M02, X3 = dms_x_M03, Y3 = dms_y_M03,  X4 = dms_x_M04, Y4 = dms_y_M04)
+    output$tab <- renderDT( {  tab_react() # %>%  select(id, nome, cpf, rua, casa, area, perim, dist_frente, dist_lateral, observacoes, obs_frente, obs_lat_dir, obs_lat_esq, obs_fundos, X1 = dms_x_M01, Y1 = dms_y_M01, X2 = dms_x_M02, Y2 = dms_y_M02, X3 = dms_x_M03, Y3 = dms_y_M03,  X4 = dms_x_M04, Y4 = dms_y_M04)
     } )
     
     output$V_tab <- renderTable(  tab_react() %>% 
@@ -369,7 +370,7 @@ shinyApp( ui = ui, server = server, options = list()) #width = 100, lauch_browse
 
 # Run the application with themer
 
-# run_with_themer(  shinyApp( ui = ui, server = server, options = list(width = 100) ) )
+# run_with_themer(  shinyApp( ui = ui, server = server))#, options = list(width = 100) ) )
 
 
 # runApp('Dabukuri_espacial')
